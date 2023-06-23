4e35f81fd84d8810f906a924327705f1f1a458da
ayekaunic <ayekaunic@gmail.com>
2023-06-23 22:33:47 +0500

    minor changes.

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected.');
+  
+  // send a message to the client
+  ws.send('Hello, client!');
+  
+  // handle incoming messages from the client
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+  });
+  
+  // handle client disconnection
+  ws.on('close', () => {
+    console.log('Client disconnected.');
+  });
+});
