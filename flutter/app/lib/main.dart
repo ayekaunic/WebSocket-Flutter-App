ff39846430fa61ec74dac464e56321fbed641807
ayekaunic <ayekaunic@gmail.com>
2023-06-26 16:15:25 +0500

    i'm the greatest there is, the greatest there was and the greatest there will ever be.
    nah blud i've been at it for 9 days w dis lemme have my moment.

--- flutter/app/lib/main.dart
+++ flutter/app/lib/main.dart
@@ -1,10 +1,12 @@
-import 'package:app/notification_api.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:web_socket_channel/io.dart';
 
 var _channel = IOWebSocketChannel.connect('ws://192.168.1.7:8080');
 TextEditingController _controller = TextEditingController();
+final FlutterLocalNotificationsPlugin _notifications =
+    FlutterLocalNotificationsPlugin();
 
 // method to send data to the server
 void _sendMessage() {
@@ -14,7 +16,15 @@ void _sendMessage() {
   }
 }
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  _notifications.initialize(initializationSettings);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -32,6 +42,7 @@ class MyApp extends StatelessWidget {
           padding: const EdgeInsets.all(23),
           child: Column(
             children: [
+              const SizedBox(height: 25),
               Form(
                 child: TextFormField(
                   controller: _controller,
@@ -54,10 +65,9 @@ class MyApp extends StatelessWidget {
                     case ConnectionState.active:
                       if (snapshot.hasData) {
                         final message = snapshot.data.toString();
-                        NotificationApi.showNotification(
+                        _showNotification(
                           title: 'Server',
                           body: message,
-                          payload: 'none',
                         );
                         return Text('Server: $message');
                       }
@@ -75,6 +85,28 @@ class MyApp extends StatelessWidget {
           child: Icon(Icons.send),
         ),
       ),
+    );
+  }
+
+  // Define the _showNotification method
+  Future<void> _showNotification(
+      {required String title, required String body}) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'channel_id',
+      'channel_name',
+      channelDescription: 'description',
+      importance: Importance.max,
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await _notifications.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
     );
   }
 }
