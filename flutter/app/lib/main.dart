8ba9987406563565846602ccd9e3751695b1de9d
ayekaunic <ayekaunic@gmail.com>
2023-06-26 16:37:21 +0500

    Final.
    Code cleaned and commented.

--- flutter/app/lib/main.dart
+++ flutter/app/lib/main.dart
@@ -3,26 +3,40 @@ import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:web_socket_channel/io.dart';
 
-var _channel = IOWebSocketChannel.connect('ws://192.168.1.7:8080');
+// Creating a WebSocket channel to connect to the server
+var _webSocketChannel = IOWebSocketChannel.connect('ws://192.168.1.7:8080');
+
+// Controller for the input text field
 TextEditingController _controller = TextEditingController();
+
+// Initialize the FlutterLocalNotificationsPlugin
 final FlutterLocalNotificationsPlugin _notifications =
     FlutterLocalNotificationsPlugin();
 
-// method to send data to the server
+// Method to send data to the server
 void _sendMessage() {
   if (_controller.text.isNotEmpty) {
-    _channel.sink.add(_controller.text);
+    _webSocketChannel.sink.add(_controller.text);
     _controller.text = '';
   }
 }
 
 void main() {
+  // Ensure that Flutter initializes the necessary bindings
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Configure the Android initialization settings for the notification plugin
   const AndroidInitializationSettings initializationSettingsAndroid =
       AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  // Create the initialization settings for the notifications plugin
   const InitializationSettings initializationSettings =
       InitializationSettings(android: initializationSettingsAndroid);
+
+  // Initialize the FlutterLocalNotificationsPlugin with the initialization settings
   _notifications.initialize(initializationSettings);
+
+  // Run the app
   runApp(const MyApp());
 }
 
@@ -31,9 +45,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Set the system UI mode to immersive sticky
     SystemChrome.setEnabledSystemUIMode(
       SystemUiMode.immersiveSticky,
     );
+
     return MaterialApp(
       title: 'WebSocket Demo',
       home: Scaffold(
@@ -42,17 +58,17 @@ class MyApp extends StatelessWidget {
           padding: const EdgeInsets.all(23),
           child: Column(
             children: [
-              const SizedBox(height: 25),
+              const SizedBox(height: 23),
               Form(
                 child: TextFormField(
                   controller: _controller,
-                  decoration:
-                      const InputDecoration(labelText: 'Send a message'),
+                  decoration: const InputDecoration(
+                      labelText: 'Send a message, server will send it back'),
                 ),
               ),
               const SizedBox(height: 23),
               StreamBuilder(
-                stream: _channel.stream,
+                stream: _webSocketChannel.stream,
                 builder: (context, snapshot) {
                   if (snapshot.hasError) {
                     return SelectableText('Error:\n${snapshot.error}');
@@ -65,6 +81,7 @@ class MyApp extends StatelessWidget {
                     case ConnectionState.active:
                       if (snapshot.hasData) {
                         final message = snapshot.data.toString();
+                        // Show a push notification when a message is received
                         _showNotification(
                           title: 'Server',
                           body: message,
@@ -88,7 +105,7 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  // Define the _showNotification method
+  // Method to show a notification
   Future<void> _showNotification(
       {required String title, required String body}) async {
     const AndroidNotificationDetails androidPlatformChannelSpecifics =
@@ -102,6 +119,7 @@ class MyApp extends StatelessWidget {
     const NotificationDetails platformChannelSpecifics =
         NotificationDetails(android: androidPlatformChannelSpecifics);
 
+    // Show the notification using the FlutterLocalNotificationsPlugin
     await _notifications.show(
       0,
       title,
